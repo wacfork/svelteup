d57c2dd7b8cb7a8f7fa69b59a4d4d3cdfa74f093
iuser <iuser.link@gmail.com>
2023-01-28 12:59:42 +0800

    v2.0.15

--- dist.sh
+++ dist.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-_DIR=$(cd "$(dirname "$0")"; pwd)
+_DIR=$(
+  cd "$(dirname "$0")"
+  pwd
+)
 
 cd $_DIR
 
@@ -8,18 +11,18 @@ set -ex
 
 npm run prepare
 
-version=$(cat package.json|jq -r '.version')
+version=$(cat package.json | jq -r '.version')
 
-add(){
-git add -u
-git commit -m v$version || true
+add() {
+  git add -u
+  git commit -m v$version || true
 }
 
 add
 
 git pull
 
-npm set unsafe-perm true
+#npm set unsafe-perm true
 npm version patch
 add
 git push
